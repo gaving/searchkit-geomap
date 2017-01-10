0d11c550151ded8cac96c2fb7f3db4149d17e1e2
Gavin Gilmour <gavin.gilmour@gmail.com>
2017-01-10 18:34:49 +0000

    bump nwb

--- nwb.config.js
+++ nwb.config.js
@@ -2,28 +2,19 @@ var path = require('path');
 
 module.exports = {
   type: 'react-component',
-  build: {
-    externals: {
-      'react': 'React'
-    },
-    global: 'GeoMap',
-    jsNext: true,
-    umd: true
+  "npm": {
+    "esModules": true,
+    "umd": {
+      "global": "GeoMap",
+      "externals": {
+        "react": "React"
+      }
+    }
   },
   webpack: {
-    extra: {
-      node: {
-        console: true,
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-      },
-      resolve: {
-        alias: {
-          'mapbox-gl/js/geo/transform': path.join(__dirname, 'node_modules/mapbox-gl/js/geo/transform'),
-          'mapbox-gl': path.join(__dirname, 'node_modules/mapbox-gl/dist/mapbox-gl.js')
-        }
-      }
+    aliases: {
+      'mapbox-gl/js/geo/transform': path.join(__dirname, 'node_modules/mapbox-gl/js/geo/transform'),
+      'mapbox-gl': path.join(__dirname, 'node_modules/mapbox-gl/dist/mapbox-gl.js')
     }
   }
 }
